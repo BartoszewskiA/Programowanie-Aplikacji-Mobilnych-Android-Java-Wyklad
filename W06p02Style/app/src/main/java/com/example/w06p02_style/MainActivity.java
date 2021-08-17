886dafaf9ba82c729c91788642872a72f7b5c5ee
BartoszewskiA <artur.bartoszewski@uthrad.pl>
2021-08-17 18:44:08 +0200

    całość  v2

---
+++ W06p02Style/app/src/main/java/com/example/w06p02_style/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.w06p02_style;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
